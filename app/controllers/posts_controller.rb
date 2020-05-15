a99bffad3bb8d5d5ebcb819196bb99fb98202031
aa1b02ab7492d696da6e8ac540ffa187ba623c88e15238d8d026f79bc5d39b3c <darrickpang@DESKTOP-02GI1R6.localdomain>
2020-05-14 20:38:21 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,13 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end
   def new
     @post = Post.new
   end
